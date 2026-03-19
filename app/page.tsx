aa2a291453e32b39acd57bf4bfbbeaf00a7f93b1
Samuel Marcos <samu.souza205@gmail.com>
2026-03-19 16:29:55 -0300

    Layout original restaurado e funcionando 100%

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,10 @@
-﻿export default function Page() { return ( <h1>Chat Brasil Online</h1> ); }
+﻿
+"use client";
+export default function Page() {
+  return (
+    <div style={{ width: "100vw", height: "100vh", margin: 0, padding: 0 }}>
+      <iframe src="/index_original.html" style={{ width: "100%", height: "100%", border: "none" }} />
+    </div>
+  );
+}
+
