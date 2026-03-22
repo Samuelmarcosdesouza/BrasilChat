5157ba1a8280fb0db556f793fe22b6842e347410
Samuel Marcos <samu.souza205@gmail.com>
2026-03-22 12:52:06 -0300

    Adicionando favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,18 @@
-﻿export default function Layout({ children }: { children: React.ReactNode }) { return (<html><body>{children}</body></html>); }
+﻿
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Chat Brasil',
+  description: 'Seu chat online',
+  icons: {
+    icon: '/favicon.ico',
+  },
+}
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="pt-br">
+      <body>{children}</body>
+    </html>
+  )
+}
